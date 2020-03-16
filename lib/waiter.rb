f7f01d51c66ca20f617ddf994af4ea3d9fe7f06d
Aman Michael <aman.david.michael@gmail.com>
2020-03-16 03:51:29 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,22 @@
 class Waiter
+  attr_accessor :name, :age, :total, :tip, :customer
+  
+  @@all = []
+  def initialize(name, age)
+    @name = name 
+    @age = age 
+    @@all<< self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+   
+  def new_meal(waiter, customer, total, tip =0)
+    Meal.new(self, customer, total, tip)
+  end 
+    
+    
+  
 
 end
